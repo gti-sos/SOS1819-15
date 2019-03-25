946e1e024f529343011bdc22c8a6f27f0b2c7063
sos1819jlgd <gomez.juanluis@gmx.es>
2019-03-25 20:59:27 +0000

    elaboracion api rest con mongodb

--- api/sportsCenters.js
+++ api/sportsCenters.js
@@ -39,4 +39,76 @@ routes.post("/sports-centers/", (req,res)=>{
     res.sendStatus(201);
 });
 
+// Load Initial Data
+
+routes.get("/sports-centers/loadInitialData",(req,res) => {
+    
+    contacts.insertMany( 
+        [{ 
+        
+        _id:1,
+        street: "Rafael Alberti",
+        name: "AA.VV El pueblo",
+        postalcode:41008,
+        startingyear: 1990,
+        surface: 7000,
+        activity:"fútbol",
+        paviment:"tierra",
+        sportfields: 7 },
+    {
+        _id:2,
+        street: "Avda La Revoltosa",
+        name: "C.D. Amate",
+        postalcode:41006,
+        startingyear: 1988,
+        surface: 8725,
+        activity: "General",
+        paviment: "Tierra y Hormigón",
+        sportfields: 3 },
+        
+    {
+        _id:3,
+        street: "Tesalónica",
+        name: "C.D. San Pablo",
+        postalcode:41007,
+        startingyear: 1990,
+        surface: 700,
+        activity: "Fútbol",
+        paviment: "Tierra y Hormigón",
+        sportfields: 5 },
+        
+    {
+        _id:4,
+        street: "Avda Virgen De La Esperanza",
+        name: "C.D. Los Bermejales",
+        postalcode:41012,
+        startingyear: 1997,
+        surface: 528,
+        activity: "Balonmano",
+        paviment: "Hormigón",
+        sportfields: 1 },
+        
+    {
+        _id:5,
+        street: "Avda Hytasa",
+        name: "C.D. Hytasa",
+        postalcode:41006,
+        startingyear: 1992,
+        surface: 6000,
+        activity: "General",
+        paviment: "Cesped , Hormigon",
+        sportfields: 4 }
+        
+        
+    ]);
+    
+    res.send("created");
+});
+    
+routes.delete("/sports-centers",(req,res) => {
+    contacts.deleteMany();
+    res.send(sportsCenters)
+});
+
+
 module.exports = routes;
