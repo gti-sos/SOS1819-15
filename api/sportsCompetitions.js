b8f73916f42ee5326946b43c98eab0dc152a70ae
sos1819pfs <pabfersan5@alum.us.es>
2019-03-26 18:50:32 +0000

    Updating API

--- api/sportsCompetitions.js
+++ api/sportsCompetitions.js
@@ -1,132 +1,184 @@
 let express = require('express');
 let routes = express.Router();
 
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://dbPablo:sossos@cluster0-s3eqj.mongodb.net/test?retryWrites=true";
+
+const client = new MongoClient(uri, {useNewUrlParser: true});
+
 var sportsCompetitions = [];
 
-routes.get("/sports-competitions/loadInitialData",(req,res) => {
-    addData();
-    res.send("created")
+client.connect(err => {
+    sportsCompetitions = client.db("sos1819-pfs").collection("competitions");
+    console.log("Connected!");
 });
 
-routes.get("/sports-competitions",(req,res) => {
-    res.send(sportsCompetitions)
+routes.get("/sports-competitions/loadInitialData", (req, res) => {
+    if (sportsCompetitions.length > 0) {
+        addData();
+        res.send("created")
+    } else {
+        res.sendStatus(409);
+    }
+    
 });
 
-routes.post("/sports-competitions",(req,res) => {
-    let newSportsCompetitions = req.body;
+routes.get("/sports-competitions/docs", (req, res) => {
+    res.redirect('https://documenter.getpostman.com/view/6901186/S17tRntf');
+});
 
-    sportsCompetitions.push(newSportsCompetitions);
+routes.get("/sports-competitions", (req, res) => {
+    sportsCompetitions.find({}).toArray((err, competitionArray) => {
+        if (err) console.log("Error: " + err);
+        res.send(competitionArray);
+    });
+});
+
+routes.post("/sports-competitions", (req, res) => {
+    let newCompetitions = req.body;
+
+    sportsCompetitions.insert(newCompetitions);
 
     res.sendStatus(201);
 });
 
-routes.delete("/sports-competitions",(req,res) => {
-    sportsCompetitions = [];
-    res.send(sportsCompetitions)
+routes.delete("/sports-competitions", (req, res) => {
+    sportsCompetitions.deleteMany();
+
+    res.sendStatus(200);
 });
 
-routes.get("/sports-competitions/:id",(req,res) => {
+routes.get("/sports-competitions/:id", (req, res) => {
 
     let id = req.params.id;
 
-    let filteredCompetitions = sportsCompetitions.filter((c) => {
-        return c.id == parseInt(id);
-    });
+    sportsCompetitions.find({"_id": parseInt(id)}).toArray((err, competitionArray) => {
 
-    if (filteredCompetitions.length >= 1){
-        res.send(filteredCompetitions[0]);
-    } else {
-        res.sendStatus(404);
-    }
-});
-
-routes.put("/sports-competitions/:id",(req,res) => {
-
-    let id = req.params.id;
-    let updatedCompetitions = req.body;
-    var found = false;
-
-    let updatedSportsCompetitions = sportsCompetitions.map((c) => {
-        if(c.id == id){
-            found = true;
-            return updatedCompetitions;
+        if (competitionArray.length == 1) {
+            res.send(competitionArray[0]);
         } else {
-            return c
+            res.sendStatus(404);
+        }
+    });
+});
+
+routes.put("/sports-competitions/:id", (req, res) => {
+
+    let id = req.params.id;
+    let updatedCompetition = req.body;
+    var myquery = {_id: parseInt(id, 10)};
+
+    sportsCompetitions.find({"_id": parseInt(id)}).toArray((err, contactsArray) => {
+
+        if (contactsArray.length == 1) {
+            sportsCompetitions.replaceOne(myquery, updatedCompetition, function (err, obj) {
+                if (err) {
+                    console.log("error: " + err);
+                    res.sendStatus(404);
+                } else {
+                    res.sendStatus(200);
+                }
+            });
+        } else {
+            res.sendStatus(404);
         }
     });
 
-    if (!found){
-        res.sendStatus(404);
-    } else {
-        sportsCompetitions = updatedSportsCompetitions;
-        res.sendStatus(200);
-    }
+
 });
 
-routes.delete("/sports-competitions/:id",(req,res) => {
+routes.delete("/sports-competitions/:id", (req, res) => {
 
     let id = req.params.id;
-    var found = false;
 
-    let updatedSportsCompetitions = sportsCompetitions.filter((c) => {
-        if(c.id == id)
-            found = true;
+    var myquery = {_id: parseInt(id, 10)};
 
-        return c.id != id;
+    sportsCompetitions.deleteOne(myquery, function (err, obj) {
+        if (err) {
+            res.sendStatus(404);
+        } else {
+            res.sendStatus(200);
+        }
     });
 
-    if (!found){
-        res.sendStatus(404);
-    } else {
-        sportsCompetitions = updatedSportsCompetitions;
-        res.sendStatus(200);
-    }
 });
 
 
-routes.post("/sports-competitions/:id",(req,res) => {
+routes.post("/sports-competitions/:id", (req, res) => {
     res.sendStatus(405);
 });
 
-routes.put("/sports-competitions",(req,res) => {
+routes.put("/sports-competitions", (req, res) => {
     res.sendStatus(405);
 });
 
-function addData(){
-    sportsCompetitions = [
-    { 
-        id: 1,
-        year: 2018,
+function addData() {
+    sportsCompetitions.insertMany([{ 
+        _id: 1,
+        year: 2019,
         day: 4,
         month : 4,
         name: "v encuentro escolar y deportivo (programa distrito: macarena - norte)",
         sportcenter: "",
         schoolcenter: "centro virgen milagrosa",
-        initiallocation: "", 
-        endlocation: "",
         activity: "escolar",
         lengthactivity: 6,
         totaldistance: 0,
         inscriptionprice: 0,
         additionalinfo: "actividad de promoción deportiva en la que participan los ceip de los distritos Macarena y Norte."},
     {
-        id: 2,
-        year: 2018,
+        _id: 2,
+        year: 2019,
         day: 23,
         month : 3,
         name: "campeonato de andalucía de taekwondo, categoría promesas",
-        sportcenter: "c.d. amate",
+        sportcenter: "C.D. Hytasa",
         schoolcenter: "",
-        initiallocation: "",
-        endlocation: "",
         activity: "artes marciales",
         lengthactivity: 6,
         totaldistance: 0,
         inscriptionprice: 0,
-        additionalinfo: "Hora: 20:30"}
-    ]
+        additionalinfo: "Hora: 20:30"},
+    {
+        _id: 3,
+        year: 2019,
+        day: 4,
+        month : 4,
+        name: "Mini olimpiada escolar distrito Cerro-Amate",
+        sportcenter: "C.D. Hytasa",
+        schoolcenter: "",
+        activity: "Escolar",
+        lengthactivity: 6,
+        totaldistance: 0,
+        inscriptionprice: 0,
+        additionalinfo: "Impulsar y fomentar el interés por conocer diversas modalidades deportivas entre los alumnos de Educación Primaria de los colegios pertenecientes al Distrito Cerro-Amate."},
+    {
+        _id: 4,
+        year: 2019,
+        day: 4,
+        month : 4,
+        name: "Encuentro Escolar y Deportivo (Programa Distrito: Macarena - Norte)",
+        sportcenter: "",
+        schoolcenter: "CEIP Manuel Siurot",
+        activity: "Escolar",
+        lengthactivity: 6,
+        totaldistance: 0,
+        inscriptionprice: 0,
+        additionalinfo: "Actividad de promoción deportiva en la que participan los CEIP de los distritos Macarena y Norte."},
+    {
+        _id: 5,
+        year: 2019,
+        day: 19,
+        month : 5,
+        name: "Carrera popular y escolar Paque de Miraflores",
+        sportcenter: "Parque de Miraflores",
+        schoolcenter: "",
+        activity: "Atletismo",
+        lengthactivity: 3,
+        totaldistance: 10,
+        inscriptionprice: 0,
+        additionalinfo: "Salida 10Kms: 9:30 horas"}
+    ]);
 }
-
-
 
 module.exports = routes;
