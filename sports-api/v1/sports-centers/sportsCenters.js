e310c2296707ad1342fd771ec28c31e58300e093
sos1819jlgd <gomez.juanluis@gmx.es>
2019-04-02 10:21:48 +0000

    Sport-Centers Finalizado Parte 1

--- sports-api/v1/sports-centers/sportsCenters.js
+++ sports-api/v1/sports-centers/sportsCenters.js
@@ -113,7 +113,6 @@ module.exports = function(app, BASE_PATH){
             sportsCenters.find({"id": parseInt(newsportsCenters.id)}, {projection:{_id: 0 }}).toArray((err, sportscentersArray) => {
     
             if (sportscentersArray.length < 1) {
-                sportsCenters.insert(newsportsCenters);
                 res.sendStatus(201);
             } else {
                 res.sendStatus(409);
