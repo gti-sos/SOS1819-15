8f3993101d9664566249b1202329058bcf83ca98
sos1819pfs <pabfersan5@alum.us.es>
2019-05-14 15:20:28 +0000

    Adding protactor test, e2e Test

---
+++ tests/sportsCompetitions/TC01-D02.js
@@ -0,0 +1,39 @@
+describe("Automatization test from sportsCompetitions UI - D02 - ",function () {
+    
+    it("List shows more than 1 items", function (){
+        browser.get("http://localhost:8080/ui/v1/sports-competitions/");
+        var competitions = element.all(by.repeater("comp in competitions"));
+        expect(competitions.count()).toBeGreaterThan(1);
+    });
+    
+    it("List should grow after the contact creation", function (){
+        browser.get("http://localhost:8080/ui/v1/sports-competitions/#!/");
+        
+                element(by.model('newCompetition.id')).sendKeys('14');
+                element(by.model('newCompetition.year')).sendKeys('2019');
+                element(by.model('newCompetition.day')).sendKeys('1');
+                element(by.model('newCompetition.month')).sendKeys('11');
+                element(by.model('newCompetition.name')).sendKeys('Maratón de la ONCE el 1/11');
+                element(by.model('newCompetition.lengthactivity')).sendKeys('6');
+                element(by.model('newCompetition.activity')).sendKeys('Maratón');
+                element(by.model('newCompetition.totaldistance')).sendKeys('45');
+                element(by.model('newCompetition.inscriptionprice')).sendKeys('5');
+                element(by.model('newCompetition.sportcenter')).sendKeys('Sevilla Centro');
+                //element(by.model('newCompetition.schoolcenter')).sendKeys('');
+                element(by.css(".addCompetition")).click();
+                
+                var resultModal = element(by.css(".alert-msg"));
+                expect(resultModal.getText()).toContain('Se ha añadido la nueva competición.');
+
+            });
+            
+    it("delete one element ", function () {
+        browser.get("http://localhost:8080/ui/v1/sports-competitions/#!/");
+
+        element.all(by.css(".btn-eliminar")).last().click();
+        
+        var competitions = element.all(by.repeater("comp in competitions"));
+        expect(competitions.count()).toBeLessThan(14);
+    });
+});
+    
