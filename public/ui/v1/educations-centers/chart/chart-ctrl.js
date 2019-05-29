2e544f23575adb82eea491387c6902a2086fa3d2
danielramirezotero <me@danielramirez.es>
2019-05-29 22:39:15 +0200

    Actualizada datos gráfica

--- public/ui/v1/educations-centers/chart/chart-ctrl.js
+++ public/ui/v1/educations-centers/chart/chart-ctrl.js
@@ -2,42 +2,63 @@ angular
     .module("SOS1819-15App")
     .controller("ChartCtrlEducations", ["$scope", "$http", "$routeParams", "$location", "$rootScope", function ($scope, $http, $routeParams, $location, $rootScope) {
         // Set up the chart
-        Highcharts.chart('container', {
-            chart: {
-                type: 'pyramid3d',
-                options3d: {
-                    enabled: true,
-                    alpha: 10,
-                    depth: 50,
-                    viewDistance: 50
+
+        $scope.url = "/api/v1/educations-centers";
+        refresh();
+
+        function refresh() {
+            $http.get($scope.url).then(function (response) {
+                var res = JSON.stringify(response.data, null, 2);
+                if (response.data.length === 0) {
+
                 }
-            },
-            title: {
-                text: 'Highcharts Pyramid3D Chart'
-            },
-            plotOptions: {
-                series: {
-                    dataLabels: {
-                        enabled: true,
-                        format: '<b>{point.name}</b> ({point.y:,.0f})',
-                        color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black',
-                        allowOverlap: true,
-                        x: 10,
-                        y: -5
+                console.log(response.data);
+
+                var privadoCount = response.data.filter(center => center.ownership === "Privado").length;
+                var concertadoCount = response.data.filter(center => center.ownership === "Concertado").length;
+                var publicoCount = response.data.filter(center => center.ownership === "Público").length;
+                Highcharts.chart('container', {
+                    chart: {
+                        type: 'pyramid3d',
+                        options3d: {
+                            enabled: true,
+                            alpha: 10,
+                            depth: 50,
+                            viewDistance: 50
+                        }
                     },
-                    width: '60%',
-                    height: '80%',
-                    center: ['50%', '45%']
-                }
-            },
-            series: [{
-                name: 'Unique users',
-                data: [
-                    ['publico', 15654],
-                    ['privado', 4064],
-                    ['concertado', 1987]
-                ]
-            }]
-        });
+                    title: {
+                        text: 'Titularidad de los centros'
+                    },
+                    plotOptions: {
+                        series: {
+                            dataLabels: {
+                                enabled: true,
+                                format: '<b>{point.name}</b> ({point.y:,.0f})',
+                                color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black',
+                                allowOverlap: true,
+                                x: 10,
+                                y: -5
+                            },
+                            width: '60%',
+                            height: '80%',
+                            center: ['50%', '45%']
+                        }
+                    },
+                    series: [{
+                        name: 'Unique users',
+                        data: [
+                            ['Público', publicoCount],
+                            ['Privado', privadoCount],
+                            ['Concertado', concertadoCount]
+                        ]
+                    }]
+                });
+            }, function (response) {
+                $scope.dataResponse = response.status + ", " + response.statusText
+            });
+        }
+
+
 
     }]);
